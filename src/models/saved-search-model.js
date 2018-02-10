abd2b353ded7f1800d2f9af1ce39c681c50a8086
forafish <fora.luo@gmail.com>
2018-02-11 01:30:09 +0800

    use primitive type name

--- src/models/saved-search-model.js
+++ src/models/saved-search-model.js
@@ -2,11 +2,11 @@ import timestamps from 'mongoose-timestamp';
 import { plugins } from 'mostly-feathers-mongoose';
 
 const fields = {
-  title: { type: 'String', required: true  },
-  sortBy: { type: 'String' },
-  sortOrder: { type: 'String'  },
+  title: { type: String, required: true  },
+  sortBy: { type: String },
+  sortOrder: { type: String  },
   params: { type: 'Mixed' },
-  pageProvider: { type: 'String' }
+  pageProvider: { type: String }
 };
 
 export default function model (app, name) {
