0d0132a39ced0061761ac8df244e85037c6a2219
forafish <fora.luo@gmail.com>
2018-03-02 19:25:53 +0800

    replace mongoose-timestamp plugin with native timestamps

--- src/models/saved-search-model.js
+++ src/models/saved-search-model.js
@@ -1,5 +1,8 @@
-import timestamps from 'mongoose-timestamp';
 import { plugins } from 'mostly-feathers-mongoose';
+
+const options = {
+  timestamps: true
+};
 
 const fields = {
   title: { type: String, required: true  },
@@ -11,8 +14,7 @@ const fields = {
 
 export default function model (app, name) {
   const mongoose = app.get('mongoose');
-  const schema = new mongoose.Schema(fields);
-  schema.plugin(timestamps);
+  const schema = new mongoose.Schema(fields, options);
   return mongoose.model(name, schema);
 }
 
