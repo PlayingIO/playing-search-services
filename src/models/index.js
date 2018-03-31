6f664111c233b6a7fba2ec90b49b360bf4a69587
forafish <fora.luo@gmail.com>
2018-03-31 23:35:19 +0800

    const variable

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ import glob from 'glob';
 import path from 'path';
 
 // load all models
-let modelFiles = glob.sync(path.join(__dirname, './*.model.js'));
+const modelFiles = glob.sync(path.join(__dirname, './*.model.js'));
 export default Object.assign({}, ...modelFiles.map(file => {
   const name = path.basename(file, '.model.js');
   return { [name]: require(file).default };
